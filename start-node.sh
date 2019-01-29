12b34a0ff473eb1ddd745fb700ea56c63aca53ef
Shrikant Goswami <shrikant.goswami@gmail.com>
2019-01-30 03:20:01 +0530

    Add shell scripts, genesis, bootnodes files

---
+++ start-node.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! -d /work/xdcchain/XDC/chaindata ]
+then
+  wallet=$(XDC account new --password /work/.pwd --datadir /work/xdcchain | awk -v FS="({|})" '{print $2}')
+  echo "Init Genesis Block"
+  XDC --datadir /work/xdcchain init /work/genesis.json
+else
+  wallet=$(XDC account list --datadir /work/xdcchain| head -n 1 | awk -v FS="({|})" '{print $2}')
+fi
+
+input="/work/bootnodes.list"
+bootnodes=""
+while IFS= read -r line
+do
+    if [ -z "${bootnodes}" ]
+    then
+        bootnodes=$line
+    else
+        bootnodes="${bootnodes},$line"
+    fi
+done < "$input"
+
+echo "Starting nodes with $bootnodes ..."
+XDC --bootnodes ${bootnodes} --syncmode "full" --datadir /work/xdcchain --networkid 89 -port 30303 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --rpcvhosts "*" --unlock "${wallet}" --password /work/.pwd --mine --gasprice "1" --targetgaslimit "420000000" --verbosity 3 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,XDPoS
