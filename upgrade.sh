7f23fa972b094b59d9d95fd90e161fd4eb9bc16d
AnilChinchawale <anil24593@gmail.com>
2021-11-16 18:35:22 +0530

    Upgrade XDC v.1.4.3 Network.

--- upgrade.sh
+++ upgrade.sh
@@ -3,7 +3,7 @@
 echo "Upgrading XinFin Network Configuration Scripts"
 git pull
 echo "Upgrading Docker Images"
-sudo docker pull xinfinorg/xinfinnetwork:xdc_mainnet_ignoreBlock
+sudo docker pull xinfinorg/xinfinnetwork:1.4.3
 sudo docker-compose -f docker-services.yml down
 sudo docker-compose -f docker-services.yml up -d
 
