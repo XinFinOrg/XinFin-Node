72683b5d2158fc420b650d90ee519f38c746c97a
AnilChinchawale <anil24593@gmail.com>
2023-01-18 11:08:07 +0530

    update xdc-attach script and docker image.

--- testnet/xdc-attach-apothem.sh
+++
@@ -1,1 +0,0 @@
-sudo docker exec -it testnet-apothem_xinfinnetwork_1  XDC attach /work/xdcchain-testnet/XDC.ipc
---
+++ testnet/xdc-attach.sh
@@ -0,0 +1,1 @@
+sudo docker exec -it testnet_xinfinnetwork_1  XDC attach /work/xdcchain-testnet/XDC.ipc
