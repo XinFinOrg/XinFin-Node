d4c38eda539209cac82a72a55859c077a9198736
AnilChinchawale <anil24593@gmail.com>
2023-01-18 12:43:55 +0530

    Upgrading XDC Network Configuration Scripts

--- mainnet/upgrade.sh
+++ mainnet/upgrade.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Upgrading XinFin Network Configuration Scripts"
+echo "Upgrading XDC Network Configuration Scripts"
 git pull
 echo "Upgrading Docker Images"
-sudo docker pull xinfinorg/xinfinnetwork:1.4.4
+sudo docker pull xinfinorg/xinfinnetwork:v1.4.6
 sudo docker-compose -f docker-compose.yml down
 git pull
 sudo docker-compose -f docker-compose.yml up -d
