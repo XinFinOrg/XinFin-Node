7e212f01fa2a6d95cf1df0c9192ea58c07a8fed6
Wanwiset Peerapatanapokin <wanwiset25@gmail.com>
2023-08-10 13:29:09 +0400

    subnet ver

--- subnet/deployment-generator/gen.js
+++ subnet/deployment-generator/gen.js
@@ -179,7 +179,7 @@ function genSubnetNodes(machine_id, num, start_num=1) {
     config='${SUBNET_CONFIG_PATH}/subnet'+i.toString()+'.env'
     compose_profile='machine'+machine_id.toString()
     subnet_nodes[node_name] = {
-      'image': 'xinfinorg/xdcsubnets:v0.1.1',
+      'image': 'xinfinorg/xdcsubnets:latest',
       'volumes': [volume, '${SUBNET_CONFIG_PATH}/genesis.json:/work/genesis.json'],
       'restart': 'always',
       'network_mode': 'host',
@@ -196,7 +196,7 @@ function genBootNode(machine_id){
   config='${SUBNET_CONFIG_PATH}/common.env'
   machine='machine'+machine_id.toString()
   bootnode = {
-    'image': 'xinfinorg/xdcsubnets:v0.1.1',
+    'image': 'xinfinorg/xdcsubnets:latest',
     'restart': 'always',
     'env_file': config,
     'volumes': ['./bootnodes:/work/bootnodes'],
@@ -247,7 +247,7 @@ function genServices(machine_id) {
     'profiles': [machine]
   },
   puppeth = {
-    'image': 'xinfinorg/xdcsubnets:v0.1.1',
+    'image': 'xinfinorg/xdcsubnets:latest',
     'entrypoint': ['puppeth'],
     'volumes': ['./puppeth:/root/.puppeth'],
     'profiles': ['none']
