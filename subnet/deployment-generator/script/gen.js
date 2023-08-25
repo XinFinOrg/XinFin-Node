adc918063873937ab7345193d01d01f4edbc6d8f
Wanwiset Peerapatanapokin <wanwiset25@gmail.com>
2023-08-25 09:27:53 +0400

    Generator imagefix (#84)

--- subnet/deployment-generator/script/gen.js
+++ subnet/deployment-generator/script/gen.js
@@ -385,10 +385,10 @@ function genCommands(num_machines, network_name, network_id, num_subnet, keys){
     commands+=`  docker compose --env-file docker-compose.env --profile ${machine_name} up -d\n\n` //composeV2
   }
 
-  commands+=`\nmachine1:                deploy checkpoint smart contract\n`
+  commands+=`\nmachine1:                deploy checkpoint smart contract (please be mindful of docker image tag if you are not using 'latest' \n`
   commands+=`  docker run --env-file docker.env \\
     -v $(pwd)/generated/deployment.json:/app/generated/deployment.json \\
-    --entrypoint 'bash' generator ./deploy_csc.sh \n`
+    --entrypoint 'bash' xinfinorg/subnet-generator:latest ./deploy_csc.sh \n`         //how to inject version other than latest??
   commands+=`  make an edit to ./config/common.env to include values for CHECKPOINT_CONTRACT \n`
 
   commands+=`\nmachine1:                start services and frontend\n`
