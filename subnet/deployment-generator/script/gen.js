235098c359362328390db9af08b05576ab8292b8
Wanwiset Peerapatanapokin <wanwiset25@gmail.com>
2023-09-06 14:28:13 +0400

    rm warning msg

--- subnet/deployment-generator/script/gen.js
+++ subnet/deployment-generator/script/gen.js
@@ -408,7 +408,7 @@ function genCommands(num_machines, network_name, network_id, num_subnet, keys){
     commands+=`  docker compose --env-file docker-compose.env --profile ${machine_name} up -d\n\n` //composeV2
   }
                                     
-  commands+=`\nmachine1:                deploy checkpoint smart contract (please be mindful of docker image tag if you are not using 'latest')\n` // 
+  commands+=`\nmachine1:                deploy checkpoint smart contract\n` // 
   commands+=`  cd ..\n`
   commands+=`  docker run --env-file generated/common.env \\
     -v $(pwd)/generated/:/app/generated/ \\
