2900ec10e1b1fb1b0b7653f8045f855236b33ac9
Wanwiset Peerapatanapokin <wanwiset25@gmail.com>
2023-09-07 10:53:48 +0400

    bump version

--- subnet/deployment-generator/script/gen_config.js
+++ subnet/deployment-generator/script/gen_config.js
@@ -16,10 +16,10 @@ var config = {
   relayer_mode:    (process.env.RELAYER_MODE          || 'full'), //full or lite
   docker_image_name: (process.env.IMAGE_NAME          || 'xinfinorg/subnet-generator:latest'),
   version: {
-    subnet:   (process.env.VERSION_SUBNET   || 'v0.1.4'),
+    subnet:   (process.env.VERSION_SUBNET   || 'v0.1.5'),
     bootnode: (process.env.VERSION_BOOTNODE || 'v0.1.4'),
     observer: (process.env.VERSION_OBSERVER || 'latest'),
-    relayer:  (process.env.VERSION_RELAYER  || 'v0.1.3'),
+    relayer:  (process.env.VERSION_RELAYER  || 'v0.1.4'),
     stats:    (process.env.VERSION_STATS    || 'v0.1.4'),
     frontend: (process.env.VERSION_FRONTEND || 'v0.1.4')
   },
