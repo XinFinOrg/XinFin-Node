708b2027fc261c45a829194d300b159380e4078a
Wanwiset Peerapatanapokin <wanwiset25@gmail.com>
2023-09-19 17:17:36 +0400

    trig ci

--- subnet/deployment-generator/docker/start.sh
+++ subnet/deployment-generator/docker/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 node /app/script/gen.js && puppeth --file /app/generated/genesis_input.yml --out /app/generated/
 
+
 if [[ -n "${SLEEP}" ]]; then
     echo "sleep ${SLEEP}"
     sleep ${SLEEP}
