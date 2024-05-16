c57ee7f4cf2442ae3274ff1bee803968bb6bce1a
Liam <liam.icheng.lai@gmail.com>
2024-05-16 23:28:04 +0800

    backup-script (#128)
    Co-authored-by: Liam Lai <liam.lai@babylonchain.io>

--- mainnet/upgrade.sh
+++ mainnet/upgrade.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 echo "Upgrading XDC Network Configuration Scripts"
+
+mv .env .env.bak
+mv .nodekey .nodekey.bak
+
+git stash
 git pull
+
+mv .env.bak .env
+mv .nodekey.bak .nodekey
+
 echo "Upgrading Docker Images"
 sudo docker pull xinfinorg/xdposchain:v1.6.0
 sudo docker-compose -f docker-compose.yml down
