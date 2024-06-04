9edf9c11ee2a607d584a4db510515d556dbdf499
Anil Chinchawale <anil24593@gmail.com>
2024-06-04 17:39:18 +0530

    Remove Admin namespace from RPC & WS API DevNet

--- devnet/start.sh
+++ devnet/start.sh
@@ -30,7 +30,7 @@ XDC --ethstats ${netstats} --gcmode=archive \
 --datadir /work/xdcchain --networkid 551 \
 -port 30303 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 \
 --rpcport 8545 \
---rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,XDPoS \
+--rpcapi db,eth,debug,miner,net,shh,txpool,personal,web3,XDPoS \
 --rpcvhosts "*" --unlock "${wallet}" --password /work/.pwd --mine \
 --gasprice "1" --targetgaslimit "420000000" --verbosity 3 \
 --ws --wsaddr=0.0.0.0 --wsport 8555 \
