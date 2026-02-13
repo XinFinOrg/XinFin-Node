afcb83263332e094eebc5e5d555fe13babe75108
Anil Chinchawale <anil@xinfin.org>
2026-02-13 16:09:02 +0100

    fix: migrate docker-compose v1 to v2, update images and API compatibility
    - Replace docker-compose (v1) with docker compose (v2) across all scripts
    - Remove deprecated 'version' field from compose files
    - Update Docker installation scripts to use Docker Compose v2 plugin
    - Fix API version compatibility (minimum 1.44)
    - Update documentation with Docker version requirements (24.0+, v2.20+)

--- devnet/docker-up.sh
+++ devnet/docker-up.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-which docker-compose
-
-if [[ $? != 0 ]]; then
-    shopt -s expand_aliases
-    alias docker-compose='docker compose'
-fi
-
-docker-compose -f docker-compose.yml up -d --build --force-recreate
+docker compose -f docker-compose.yml up -d --build --force-recreate
--- setup/upgrade.sh
+++ setup/upgrade.sh
@@ -4,7 +4,6 @@ echo "Upgrading XinFin Network Configuration Scripts"
 git pull
 echo "Upgrading Docker Images"
 sudo docker pull xinfinorg/xinfinnetwork:1.4.4
-sudo docker-compose -f docker-compose.yml down
+docker compose -f docker-compose.yml down
 git pull
-sudo docker-compose -f docker-compose.yml up -d
-
+docker compose -f docker-compose.yml up -d
